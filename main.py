344881b03fdfef5c90029b9f4e82fbe4944f3f01
john <john@jpatmore.plus.com>
2020-12-09 22:59:41 +0000

    Answer checked for validity. Appropriate Series extracted from data. Added player messages.

--- main.py
+++ main.py
@@ -1,19 +1,68 @@
 import turtle
+import pandas
+from messenger import Messenger
 
 image = "blank_states_img.gif"
+csv = "50_states.csv"
 
+
+# Set up the Screen
+# =================
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 screen.setup(width=725, height=491)  # Sets the actual window size
-
 # screen.screensize(canvwidth=725, canvheight=491)  # Sets the size of the canvas the turtles are drawing on
-# screen.bgpic(image)
 
-screen.addshape(image)
-turtle.shape(image)
+screen.bgpic(image)
+# screen.addshape(image)
+# turtle.shape(image)
 
-answer = screen.textinput(title="Answer", prompt="Name a U.S. state :").title()
-print(answer)
+# player_msg displays information to the player
+player_msg = Messenger(
+    fontcolor="red",
+    fontsize=24,
+    fonttype="bold italic",
+)
+
+# state_name displays the state names on the map
+state_name = Messenger(
+    fontcolor="black",
+    fontsize=10,
+    fonttype="normal",
+)
+
+# Read the CSV into a Pandas DataFrame
+df = pandas.read_csv(csv)
+# print(df.head(3))
+"""
+             state    x    y
+0          Alabama  139  -77
+1           Alaska -204 -170
+2          Arizona -203  -40
+"""
+
+
+# Game Loop
+# =========
+game_on = True
+total = len(df.index)  # Total number of states
+correct = 0  # Number of correct answers given
+while game_on:
+    answer = screen.textinput(title=f"{correct}/{total} States Correct", prompt="Name a U.S. state :")
+    # Prevent .title() crash if cancelled or nothing is input
+    if answer is None:
+        player_msg.display_message("Incorrect!\nTry Again", 2)
+    else:
+        # Extract the corresponding Series from the DataFrame
+        row = df[df["state"] == answer.title()]
+
+        # Check that the answer given is valid
+        if row.empty:
+            player_msg.display_message("Incorrect!\nTry Again", 2)
+        else:
+            print(row["x"])
+
+        game_on = False
 
 turtle.mainloop()  # Keep the window open when the program ends
 
---
+++ messenger.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+from time import sleep
+
+
+class Messenger(Turtle):
+    def __init__(self, fontcolor, fontsize, fonttype):
+        super(Messenger, self).__init__()
+        self.pu()
+        self.hideturtle()
+        self.setposition(0, 0)
+        self.pencolor(fontcolor)
+        self.font = ("Comic Sans", fontsize, fonttype)
+
+    def display_message(self, message, time):
+        """Displays message for a time.
+        If time <= 0 then the message is not cleared."""
+        self.clear()  # Clear any previous message
+        self.write(arg=message, move=False, align="center", font=self.font)
+        if time > 0:
+            sleep(time)
+            self.clear()
