b14070a253b91d1c1c6f49415414ffe7988eafcb
john <john@jpatmore.plus.com>
2020-12-10 01:52:20 +0000

    Real GIF added

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ csv = "50_states.csv"
 
 
 def extract_and_display_state(row):
-    # Reset the row index to zero (old index retained in a new column)
+    """Extract data from the Pandas Series,
+    and write the state name to the appropriate location on the screen"""
+    # Reset the row index to zero (the old index is retained in a new Index column)
     row1 = row.reset_index()
     state_name = row1.at[0, "state"]
     state_x = int(row1.at[0, "x"])  # Convert numpy.int64 to int
     state_y = int(row1.at[0, "y"])
-    state_msg.display_state(message=state_name, position=(state_x, state_y))
+    state_msg.message_position(message=state_name, position=(state_x, state_y))
 
 
 # Set up the Screen
@@ -36,7 +38,7 @@ player_msg = Messenger(
 # state_msg displays the state names on the map
 state_msg = Messenger(
     fontcolor="black",
-    fontsize=10,
+    fontsize=8,
     fonttype="normal",
 )
 
@@ -52,11 +54,12 @@ score = 0  # Number of correct answers given
 correct_states = []
 
 while game_on:
+    # Get the player answer
     answer = screen.textinput(title=f"{score}/{total} States Correct", prompt="Name a U.S. state :")
 
     # Prevent .title() crash if cancelled or nothing is input
     if answer is None:
-        player_msg.display_message("Incorrect!\nTry Again.", 2)
+        player_msg.message_time("Please type a state name.\nTry Again.", 2)
     else:
         answer = answer.title()  # Convert to Title Case
 
@@ -65,12 +68,12 @@ while game_on:
 
         # Check that the answer given is valid
         if series.empty:
-            player_msg.display_message("Incorrect!\nTry Again.", 2)
+            player_msg.message_time("Incorrect!\nTry Again.", 2)
         else:
 
             # Check if already answered
             if answer in correct_states:
-                player_msg.display_message(f"You already got\n{answer}.\nTry Again.", 2)
+                player_msg.message_time(f"You already got\n{answer}.\nTry Again.", 2)
             else:
                 score += 1
                 correct_states.append(answer)
@@ -80,6 +83,6 @@ while game_on:
                 if score == total:
                     game_on = False
                     player_msg.pencolor("green")
-                    player_msg.display_message("Completed.\nWell Done!", 5)
+                    player_msg.message_time("Completed.\nWell Done!", 5)
 
 turtle.mainloop()  # Keep the window open when the program ends
--- messenger.py
+++ messenger.py
@@ -4,6 +4,7 @@ from time import sleep
 
 class Messenger(Turtle):
     def __init__(self, fontcolor, fontsize, fonttype):
+        """Turtle Class to enable writing messages to the screen"""
         super(Messenger, self).__init__()
         self.pu()
         self.hideturtle()
@@ -11,15 +12,18 @@ class Messenger(Turtle):
         self.pencolor(fontcolor)
         self.font = ("Comic Sans", fontsize, fonttype)
 
-    def display_message(self, message, time):
-        """Displays message for a time.
-        If time <= 0 then the message is not cleared."""
+    def message_time(self, message, time):
+        """Displays message for the given time.
+        Previous message are cleared, which allows messages to be written to the same location on screen.
+        If time <= 0 then the message is not cleared until the next message is written."""
         self.clear()  # Clear any previous message
         self.write(arg=message, move=False, align="center", font=self.font)
         if time > 0:
             sleep(time)
             self.clear()
 
-    def display_state(self, message, position):
+    def message_position(self, message, position):
+        """Displays message at the given position.
+        Previous messages are NOT cleared."""
         self.setposition(position)
         self.write(arg=message, move=False, align="center", font=self.font)
